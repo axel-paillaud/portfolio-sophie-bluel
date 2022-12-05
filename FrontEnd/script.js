a9d6659aa640308c9b8206b0517918a58d689ac3
Axel <a.paillaud75@gmail.com>
2022-12-05 06:06:45 +0100

    add attribute to images works

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,12 +1,15 @@
-function createWorkElt(gallery, value) {
+function addWorkElt(gallery, value, relativeUrlImg) {
     let figure = document.createElement("figure");
     let img = document.createElement("img");
     let figcaption = document.createElement("figcaption");
 
-    gallery.appendChild(figure).appendChild(img).setAttribute("src", value.imageUrl);
+    //Add image, attribute and title to the DOM
+    gallery.appendChild(figure).appendChild(img).setAttribute("src", '../Backend' + relativeUrlImg);
+    img.setAttribute('alt', value.title);
     gallery.appendChild(figure).appendChild(figcaption).innerHTML = value.title;
 }
 
+//Get works from backend in JSON format
 fetch("http://localhost:5678/api/works")
     .then(function(res) {
         if (res.ok) {
@@ -16,11 +19,10 @@ fetch("http://localhost:5678/api/works")
     .then(function(value) {
         value.forEach(value => {
             let gallery = document.getElementsByClassName("gallery")[0];
-            createWorkElt(gallery, value);
+            const relativeUrlImg = new URL(value.imageUrl).pathname;
+            addWorkElt(gallery, value, relativeUrlImg);
         });
-        console.log(value);
     })
     .catch(function(err) {
         console.log("Une erreur est survenue");
     });
-
