19308b999c07577ab479e8b593e08e192f73a781
Axel Paillaud <a.paillaud75@gmail.com>
2022-12-06 10:19:59 +0100

    login.js : fetch to login id ok

---
+++ FrontEnd/assets/js/login.js
@@ -0,0 +1,34 @@
+const loginURI = "http://localhost:5678/api/users/login";
+
+//fake user for test, we have to get the input from the login form
+const user = {
+    email: 'sophie.bluel@test.tld',
+    password: 'S0phie'
+};
+
+const form = document.getElementsByClassName("login")[0].elements;
+
+form["submit-login"].addEventListener('click', function() {
+    fetch(loginURI, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+    .then(function(res) {
+        if (res.ok) {
+            return res.json();
+        }
+        else {
+            console.log("error");
+            return 0;
+        }
+    })
+    .then(function(value) {
+        console.log(value);
+    })
+    .catch(function(err) {
+        console.log(err);
+    })
+});
