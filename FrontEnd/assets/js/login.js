b6885ae8f735e78ff3af6d9f3bf4e9baab938cb2
Axel <a.paillaud75@gmail.com>
2022-12-09 18:16:26 +0100

    login form

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,20 +1,33 @@
 const loginURI = "http://localhost:5678/api/users/login";
+const form = document.getElementsByClassName("login")[0].elements;
 
 //fake user for test, we have to get the input from the login form
-const user = {
+const sophie = {
     email: 'sophie.bluel@test.tld',
     password: 'S0phie'
 };
 
-const form = document.getElementsByClassName("login")[0].elements;
+function getUserLog() {
+    let email = form["email"].value;
+    let password = form["password"].value;
+
+    const user = {
+        email: email,
+        password: password
+    }
+
+    console.log(user);
+    return user;
+}
 
 form["submit-login"].addEventListener('click', function() {
+    getUserLog();
     fetch(loginURI, {
         method: 'POST',
         headers: {
             'Content-type': 'application/json'
         },
-        body: JSON.stringify(user)
+        body: JSON.stringify(sophie)
     })
     .then(function(res) {
         if (res.ok) {
@@ -26,7 +39,7 @@ form["submit-login"].addEventListener('click', function() {
         }
     })
     .then(function(value) {
-        console.log(value.token);
+        sessionStorage.setItem("token", value.token);
     })
     .catch(function(err) {
         console.log(err);
