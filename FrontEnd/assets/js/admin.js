1dffcaf62bbd79ff0d7d5316c820e502ad72d4be
Axel <a.paillaud75@gmail.com>
2022-12-12 06:27:07 +0100

    modal windows

--- FrontEnd/assets/js/admin.js
+++ FrontEnd/assets/js/admin.js
@@ -11,29 +11,35 @@ function logOut(element) {
     })   
 }
 
-function headerEditionMode(element) {
-    let span = document.createElement('span');
-    let icon = document.createElement('i');
+function addHeaderEditionMode(element) {
     let button = document.createElement('button');
+    let icon = document.createElement('i');
+    let span = document.createElement('span');
 
     element.classList.add('header-edition');
 
     element.appendChild(span)
     .appendChild(icon)
     .classList.add('fa-solid', 'fa-pen-to-square');
-
     icon.insertAdjacentHTML('afterend', 'Mode édition');
+
     element.appendChild(button).innerHTML = "publier les changements";
+}
+
+function addEditProject() {
+    let element = document.getElementById('edit-project');
+    let icon = document.createElement('i');
+
+    element.appendChild(icon).classList.add('fa-solid', 'fa-pen-to-square');
 }
 
 if (token != null) {
     console.log("You are logged in as administrator");
     let login = document.querySelector("nav ul li a[href='login.html']");
     let headerEditionElt = document.getElementById('header-edition-mode');
-    console.log(headerEditionElt);
-
 
     changeInnerHtml(login, "logout");
     logOut(login);
-    headerEditionMode(headerEditionElt);
+    addHeaderEditionMode(headerEditionElt);
+    addEditProject();
 };
