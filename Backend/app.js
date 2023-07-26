98de3e4b744aab9b1c57f9af019fea0dd2184df6
axel-paillaud <contact@axelweb.fr>
2023-07-26 12:41:07 +0200

    add access control to cross origin

--- Backend/app.js
+++ Backend/app.js
@@ -9,6 +9,14 @@ const swaggerDocs = yaml.load('swagger.yaml')
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
 app.use(express.urlencoded({ extended: true }))
 app.use(helmet());
 app.use('/images', express.static(path.join(__dirname, 'images')))
