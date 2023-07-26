672f10f12dab5fb5684adc5e20443b2f93636577
axel-paillaud <contact@axelweb.fr>
2023-07-26 13:51:42 +0200

    remove cors option, enable app.use(cors());

--- Backend/app.js
+++ Backend/app.js
@@ -7,15 +7,8 @@ const swaggerUi = require('swagger-ui-express')
 const yaml = require('yamljs')
 const swaggerDocs = yaml.load('swagger.yaml')
 const app = express()
-//app.use(cors())
+app.use(cors())
 app.use(express.json())
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
 
 app.use(express.urlencoded({ extended: true }))
 app.use(helmet());
